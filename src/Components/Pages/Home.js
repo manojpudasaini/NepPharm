c2801d05f0a95433f18e30971090feeec908fd2f
manojpudasaini <mpudasaini17@gmail.com>
2021-04-12 18:40:20 +0545

    adding carousel

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import "./home.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+import Img1 from "../../Assets/1.jpg";
+import Img2 from "../../Assets/2.jpg";
+import Img3 from "../../Assets/3.jpg";
 
 const Home =()=>{
 return(
-    <h1>This is Home Page</h1>
+    <div className="carouselContainer">   
+    <Carousel showThumbs={false} showStatus={false} >
+    <div>
+        <img src={Img1}/>
+        <p>Legend 1</p>
+    </div>
+    <div>
+        <img src={Img2} />
+        <p>Legend 2</p>
+    </div>
+    <div>
+        <img src={Img3} />
+        <p>Legend 3</p>
+    </div>
+</Carousel>
+ </div>
 )
 }
 
--- src/Components/Pages/SignIn.js
+++ src/Components/Pages/SignIn.js
@@ -1,9 +1,67 @@
-import React from 'react';
-
-const SignIn =()=>{
-return(
-    <h1>This is SignIn Page</h1>
-)
+import { Form, Button, Input, message, notification } from "antd";
+import { auth } from "../utils/firebase";
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+export default function IndexPage() {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+  const handleFormSubmit = async () => {
+    try {
+      await auth.createUserWithEmailAndPassword(user.email, user.password);
+      message.success("user created");
+      const users = auth.currentUser;
+      console.log(users);
+      await users.sendEmailVerification({ url: "http://localhost:3000" });
+      notification.success({
+        message: "Verification mail sent to",
+        description: user?.email
+      });
+    } catch (e) {
+      console.log("error", e?.message);
+      message.error(e?.message);
+    }
+  };
+  return (
+    <div
+      style={{
+        marginTop:50,
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Form
+        onFinish={handleFormSubmit}
+        style={{ maxWidth: "500px", height: "100vh" }}
+        layout="vertical"
+      >
+        <Form.Item label="Email Address">
+          <Input
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            value={user.email}
+            type="email"
+          />
+        </Form.Item>
+        <Form.Item label="password">
+          <Input.Password
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            type="email"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            style={{ marginTop: "20px" }}
+            htmlType="submit"
+            type="primary"
+          >
+            Create User
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
-
-export default SignIn;
